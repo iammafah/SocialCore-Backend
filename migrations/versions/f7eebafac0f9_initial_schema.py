fa997d897934d20a273bc13f584093a92309ca02
iammafah <222110951+iammafah@users.noreply.github.com.>
2026-01-28 21:59:33 +0530

    initial migration

--- migrations/versions/f7eebafac0f9_initial_schema.py
+++ migrations/versions/f7eebafac0f9_initial_schema.py
@@ -1,8 +1,8 @@
-"""initial tables
+"""initial schema
 
-Revision ID: fda0861b36a5
+Revision ID: f7eebafac0f9
 Revises: 
-Create Date: 2026-01-26 19:10:36.005333
+Create Date: 2026-01-28 20:19:19.625875
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'fda0861b36a5'
+revision = 'f7eebafac0f9'
 down_revision = None
 branch_labels = None
 depends_on = None
